003fc2c2451dc3fd18765cae5552b017d4c96af1
Shinri Horiwa <horoiwa195@gmail.com>
2019-07-12 00:56:05 +0900

    Add overlap prediction

---
+++ unet3/predict.py
@@ -0,0 +1,151 @@
+import glob
+import os
+import shutil
+import copy
+import warnings
+warnings.filterwarnings('ignore')
+
+from PIL import Image
+import numpy as np
+import click
+
+from config import (IMAGE_COLORMODE, MASK_COLORMODE, MASK_USECOLORS,
+                    SAMPLE_SIZE, TARGET_SIZE, FRAME_SIZE)
+from models import load_unet
+
+
+@click.command()
+@click.option('--folder', '-f', required=True,
+              help='Data folder path',
+              type=click.Path(exists=True))
+@click.option('--outdir', '-o', default='__unet__',
+              help='output folder path',
+              type=click.Path(exists=True))
+@click.option('--padding', '-p', is_flag=True,
+              help='padding predict results')
+def main(folder, outdir, padding):
+    folder_path = os.path.abspath(folder)
+    outdir = os.path.abspath(outdir)
+    predict(folder_path, outdir, padding)
+
+
+def predict(folder_path, outdir, padding):
+    weights_dir = os.path.join(outdir, '__checkpoints__')
+    hdfname = os.path.join(weights_dir, 'model_1.hdf5')
+
+    results_dir = os.path.join(outdir, 'results')
+    if os.path.exists(results_dir):
+        shutil.rmtree(results_dir)
+    os.makedirs(results_dir)
+
+    images = glob.glob(os.path.join(folder_path, '*.jpg'))
+    model = load_unet(weights=hdfname)
+
+    for image_path in images:
+        image = load_image(image_path)
+        image_results = inference(image, model, padding)
+
+        if True:
+            image = Image.fromarray(image_results*255)
+            image.save("./test.jpg")
+        break
+
+
+def load_image(image_path):
+    image = Image.open(image_path)
+    image = image.resize(TARGET_SIZE, Image.LANCZOS)
+    image = np.array(image)
+    return image
+
+
+def inference(image, model, padding=True):
+    frame = FRAME_SIZE
+    if MASK_COLORMODE == 'L':
+        image_results = np.zeros((image.shape[0], image.shape[1], 1))
+    elif MASK_COLORMODE == 'RGB':
+        image_results = np.zeros((image.shape[0], image.shape[1], 3))
+        #image_results = np.zeros((image.shape[0], image.shape[1], len(MASK_USECOLORS)))
+    else:
+        raise Exception('Unexpected maskusecolors')
+
+    height = SAMPLE_SIZE[0]
+    width = SAMPLE_SIZE[1]
+
+    x_lim = TARGET_SIZE[0]
+    y_lim = TARGET_SIZE[1]
+
+    assert x_lim % frame == 0
+    assert y_lim % frame == 0
+    assert x_lim % width == 0
+    assert y_lim % height == 0
+
+    x_list = list(range(0, x_lim, frame))
+    y_list = list(range(0, y_lim, frame))
+
+    for i in range(len(x_list)):
+        for j in range(len(y_list)):
+            x = x_list[i]
+            y = y_list[j]
+            x_s = x if x == 0 else x-frame
+            y_s = y if y == 0 else y-frame
+            x_e = x if x == x_list[-1] else x_s + width
+            y_e = y if y == y_list[-1] else y_s + height
+
+            if (x_e > x_lim) or (y_e > y_lim):
+                continue
+
+            image_temp = copy.deepcopy(image[y_s:y_e, x_s:x_e])
+            print()
+            print("DEBUG0", y_lim, x_lim)
+            print("DEBUG1", y_s, y_e, x_s, x_e)
+            print("DEBUG2", y, x)
+            print("DEBUG3", image_temp.shape)
+            print()
+
+            if (x_e > x_lim) or (y_e > y_lim):
+                continue
+
+            if IMAGE_COLORMODE == 'L':
+                image_temp = image_temp.reshape(SAMPLE_SIZE+(1,))
+                image_temp = image_temp.reshape((1,)+SAMPLE_SIZE+(1,))
+            elif IMAGE_COLORMODE == 'RGB':
+                image_temp = image_temp.reshape(SAMPLE_SIZE+(3,))
+                image_temp = image_temp.reshape((1,)+SAMPLE_SIZE+(3,))
+
+            pred = model.predict(image_temp)
+            if MASK_COLORMODE == 'L':
+                pred = pred.reshape(SAMPLE_SIZE+(1,))
+            elif MASK_COLORMODE == 'RGB':
+                pred = pred.reshape(SAMPLE_SIZE+(pred.shape[-1],))
+            else:
+                raise Exception('Unexpected Error')
+            print("Prediction", pred.shape)
+
+            if padding:
+                if x == 0 and y == 0:
+                    image_results[0:frame, 0:frame, :] = pred[0:frame, 0:frame, :]
+                elif x == 0 and y_e == y_lim:
+                    image_results[-frame:, 0:frame, :] = pred[-frame:, 0:frame, :]
+                elif y == 0 and x_e == x_lim:
+                    image_results[0:frame, -frame:, :] = pred[0:frame, -frame:, :]
+                elif x_e == x_lim and y_e == y_lim:
+                    image_results[-frame:, -frame:, :] = pred[-frame:, -frame:, :]
+
+                if x == 0:
+                    image_results[y_s+frame:y_e-frame, x_s:x_s+frame, :] = pred[frame:-frame, 0:0+frame, :]
+                elif x_e == x_lim:
+                    image_results[y_s+frame:y_e-frame, x_e-frame:x_e, :] = pred[frame:-frame, -frame:, :]
+
+                if y == 0:
+                    image_results[y_s:y_s+frame, x_s+frame:x_e-frame, :] = pred[0:frame, frame:-frame, :]
+                elif y_e == y_lim:
+                    image_results[y_e-frame:y_e, x_s+frame:x_e-frame, :] = pred[-frame:, frame:-frame, :]
+
+            image_results[y_s+frame:y_e-frame, x_s+frame:x_e-frame, :] = pred[frame:-frame, frame:-frame, :]
+
+    return image_results
+
+
+
+if __name__ == '__main__':
+    main()
