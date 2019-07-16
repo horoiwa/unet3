10e121efcf15ec91ac4c0c6bfd96a6924ebc096c
Shinri Horiwa <horoiwa195@gmail.com>
2019-07-17 00:31:08 +0900

    Use bincross

--- unet3/config.py
+++ unet3/config.py
@@ -7,8 +7,8 @@ MASK_COLORMODE = 'RGB'
 
 """RGB setting
 """
-MASK_USECOLORS = 'RB'
-BG_COLOR = False
+MASK_USECOLORS = 'RGB'
+BG_COLOR = True
 BACKGROUND_COLOR = [0, 1, 0]
 
 """ 'tversky' or 'categorical cross entropy' or 'binary_crossentropy'
@@ -28,7 +28,7 @@ TRAIN_STEPS = 100
 VALID_STEPS = 50
 EPOCHS = 30
 
-EA_EPOCHS = 5
+EA_EPOCHS = 6
 
 #: 基本的にはこの設定値なら影響がない
 PCA_COLOR_RANGE = (-0.2, 0.2)
@@ -36,18 +36,18 @@ PCA_COLOR_RANGE = (-0.2, 0.2)
 DATA_GEN_ARGS = dict(
     rescale=1./255,
     rotation_range=40,
-    width_shift_range=0.1,
-    height_shift_range=0.1,
-    shear_range=0.0,
-    zoom_range=0.1,
+    width_shift_range=0.05,
+    height_shift_range=0.05,
+    shear_range=0.05,
+    zoom_range=0.05,
     vertical_flip=True,
     horizontal_flip=True,
-    cval=0,
-    fill_mode='constant')
+    fill_mode='nearest')
 
 """
 基本はグレスケ入力-L/RGB出力を想定
 未検証：RGB入力
 
 UpSampling2D使用は出力の格子模様を防ぐ
+領域塗分けは他ラベル問題ととらえbinary_crossentorpyを使う
 """
